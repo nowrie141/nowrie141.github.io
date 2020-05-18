3c52da8a3512d10081ef78c4157fc2fcfc468d66
Ismael Abu-jadur García <i62abgai@uco.es>
2020-05-18 07:54:18 +0200

    Fixed responsible, new images added

--- main.js
+++ main.js
@@ -7,4 +7,18 @@ $(document).ready(function (){
         
     });
 
+    $('.top-nav .nav-link').on('click', function () {
+
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+        
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+
+        $('html, body').animate( {
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 2000);
+        
+    });
 });
