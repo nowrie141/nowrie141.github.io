5a64dba4b91125da0342793207b8627a67904d0e
Ismael Abu-jadur García <i62abgai@uco.es>
2020-08-07 20:31:19 +0100

    Fixed up button, disabled scroll when menu opened

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
-menu = document.querySelector('.menu')
-menu_toggler = document.querySelector('.menu-toggler')
-top_nav = document.querySelector('.top-nav')
-nav_links = Array.from(document.querySelectorAll('.nav-link'))
-id_sections = Array.from(document.querySelectorAll('nav a[href*="#"]'))
-up = document.querySelector('#up')
+const menu = document.querySelector('.menu')
+const menu_toggler = document.querySelector('.menu-toggler')
+const top_nav = document.querySelector('.top-nav')
+const nav_links = Array.from(document.querySelectorAll('.nav-link'))
+const id_sections = Array.from(document.querySelectorAll('nav a[href*="#"]'))
+const up = document.querySelector('#up')
 
+// Fix button to go up
+function fixUp () {
+  if (window.scrollY >= top_nav.offsetHeight) {
+    document.body.classList.add('fixed-menu')
+  } else {
+    document.body.classList.remove('fixed-menu')
+  }
+}
+
+window.addEventListener('scroll', fixUp)
+
+let disabledScroll = false
+
+function disableScroll () {
+  // Get the current page scroll position
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+  // if any scroll is attempted, set this to the previous value
+  window.onscroll = function () {
+    window.scrollTo(scrollLeft, scrollTop)
+  }
+  disabledScroll = true
+}
+
+function enableScroll () {
+  window.onscroll = function () {}
+  disabledScroll = false
+}
+
+// Open and close the menu adding the class open
 function removeOpen () {
   if (
     top_nav.classList.contains('open') &&
@@ -13,17 +43,24 @@ function removeOpen () {
     menu_toggler.classList.remove('open')
     top_nav.classList.remove('open')
   }
+  enableScroll()
 }
 
 menu.addEventListener('click', () => {
   menu_toggler.classList.toggle('open')
   top_nav.classList.toggle('open')
+  if (disabledScroll == true) {
+    enableScroll()
+  } else {
+    disableScroll()
+  }
 })
 
 top_nav.addEventListener('click', removeOpen)
 
 nav_links.forEach(nav_link => nav_link.addEventListener('click', removeOpen))
 
+// Scroll into each section
 id_sections.forEach(id_section =>
   id_section.addEventListener('click', () => {
     section = document.querySelector(`${id_section.hash}`)
@@ -31,6 +68,7 @@ id_sections.forEach(id_section =>
   })
 )
 
+// Make the scroll smooth
 up.addEventListener('click', () => {
   window.scrollTo({
     top: 0,
